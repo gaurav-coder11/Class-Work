4878871f0e4e71fe8ad9af4feab2afd358458552
Gaurav Parwate <144258110+gaurav-coder11@users.noreply.github.com>
2023-09-11 11:04:00 +0530

    Day One

---
+++ Day1/About_Me.java
@@ -0,0 +1,13 @@
+//create class
+class About_Me{
+public static void main(String[] args){
+	//print statements
+	System.out.println("My Name is - Gaurav Lokchand Parwate");
+	System.out.println("At:-Gondia");
+	System.out.println("Digree:B.E");
+	System.out.println("Stream:COMPUTER ENGINEERING");
+	System.out.println("PASSING YEAR:2023");
+	System.out.println("COLLEGE:MIET,Bhandara");
+	System.out.println("Thank You");
+}
+}
---
+++ Day1/Add.java
@@ -0,0 +1,18 @@
+//Adding Two Numbers
+import java.util.Scanner;
+class ArithmaticOp{
+public static void main(String[] args){
+					//program to demonstrate use of arithmatic operations	
+	int a,b,su;      //declaretion of veriables
+	
+	Scanner sc.=new Scanner(Sysyem.in);   //initialize a Scanner
+	
+	System.out.println("Pleare Two Number");
+	
+	a=sc.nextInt();  	//store the value throw user is put
+	b=sc.nextInt();  	//store the value throw user is put
+	
+	su=a+b;
+	System.out.println("Your Result Is:-"+su);
+}
+}
